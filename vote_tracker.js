7db99001db135b0e12ad83f12abcbce2f5f8b242
Nick Kuhn <nickuhn@gmail.comn>
2015-05-26 20:13:20 -0700

    Fixed voting bug by adding boolean

--- vote_tracker.js
+++ vote_tracker.js
@@ -1,9 +1,10 @@
 $(function () {
   var kittens = [];
+  var voteSubmitted = false;
 
   function Kitten(path) {
     this.source = path;
-    this.votes = Math.floor(Math.random() * 1000);
+    this.votes = 0;
   }
 
   var chooseRandomPics = function() {
@@ -15,37 +16,43 @@ $(function () {
     kittens.push(new Kitten('contestants/' + i + '.jpg'));
   }
 
+
   $('.voteable').on('click', function(){
-    var displayVotes = 0;
-    var hateVotes = 0;
-    var catOneId = $(this).children().attr('src');
-    var catTwoId = $(this).siblings().children().attr('src');
-    for (var i = 0; i < kittens.length; i++) {
-      if (kittens[i].source == catTwoId) {
-        hateVotes = kittens[i].votes;
+    if (!voteSubmitted) {
+      voteSubmitted = true;
+      var displayVotes = 0;
+      var hateVotes = 0;
+      var catOneId = $(this).children().attr('src');
+      var catTwoId = $(this).siblings().children().attr('src');
+      for (var i = 0; i < kittens.length; i++) {
+        if (kittens[i].source == catTwoId) {
+          hateVotes = kittens[i].votes;
+        }
       }
-    }
-    for (var j = 0; j < kittens.length; j++) {
-      if (kittens[j].source == catOneId) {
-        kittens[j].votes ++;
-        displayVotes = kittens[j].votes;
+      for (var j = 0; j < kittens.length; j++) {
+        if (kittens[j].source == catOneId) {
+          kittens[j].votes ++;
+          displayVotes = kittens[j].votes;
+        }
       }
+      $(this).find('p').text('Happy and cuddled');
+      $(this).siblings().find('p').text('So sad and lonely.');
+      $(this).find('img').css({'border': '15px solid #CC4452'});
+      $(this).siblings().find('img').css({'border': '15px solid #F9E4AD'});
+      $(this).append('<h3 id="cat-love">I have ' + displayVotes + ' vote(s). I feel so loved.</h3>');
+      $(this).siblings('div').append('<h3 id="cat-hate"> You may not love me, but ' + hateVotes + ' people with better taste do!');
+      $('.new-cat-button').fadeIn(500);
     }
-    $(this).find('p').text('Happy and cuddled');
-    $(this).siblings().find('p').text('So sad and lonely.');
-    $(this).find('img').css({'border': '15px solid #CC4452'});
-    $(this).siblings().find('img').css({'border': '15px solid #F9E4AD'});
-    $(this).append('<h3 id="cat-love">I have ' + displayVotes + ' vote(s). I feel so loved.</h3>');
-    $(this).siblings('div').append('<h3 id="cat-hate"> You may not love me, but ' + hateVotes + ' people with better taste do!');
-    $('.new-cat-button').fadeIn(500);
+
   });
 
   $('.new-cat-button').on('click', function(){
-    $('.new-cat-button').hide();
-    $('#cat-love').remove();
-    $('#cat-hate').remove();
+    $(this).hide();
+    $('#cat-love, #cat-hate').remove();
     $('img').css({'border': '15px solid #F9E4AD'});
-    $('.voteable').on('click');
+    $('.cat-caption').text('Click to vote for me!');
+    voteSubmitted = false;
+
     var pickCat1 = chooseRandomPics();
     var pickCat2 = chooseRandomPics();
     while(pickCat1 == pickCat2){
@@ -53,7 +60,6 @@ $(function () {
     }
     $('.cat1').attr('src', pickCat1);
     $('.cat2').attr('src', pickCat2);
-    $('.cat-caption').text('Click to vote for me!');
   });
 
 });
